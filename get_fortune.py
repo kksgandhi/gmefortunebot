c9c9f1ae2b7855735343f62fa7fd7187fc340b01
Kutub Gandhi <kksgandhi@gmail.com>
2019-09-30 23:05:07 -0500

    fix bug with unsubscriptable list

--- get_fortune.py
+++ get_fortune.py
@@ -4,7 +4,7 @@ FORTUNE_URL  = 'https://helloacm.com/api/fortune'
 NUM_FORTUNES = 5
 
 api_responses = [requests.get(FORTUNE_URL) for _ in range(NUM_FORTUNES)]
-fortunes      = map(lambda req: req.json(), api_responses)
+fortunes      = list(map(lambda req: req.json(), api_responses))
 
 for index, fortune in enumerate(fortunes):
     print(f"{index + 1}: {fortune}")
